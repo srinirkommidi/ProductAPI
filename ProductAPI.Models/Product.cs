f532fd9a6eafbfae200425d7695476b63fc11fd5
srinirkommidi <srinirkommidi@gmail.com>
2025-10-07 20:54:00 +0530

    Add project files.

---
+++ ProductAPI.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductAPI.ProductAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductCost { get; set; }
+    }
+
+
+}
