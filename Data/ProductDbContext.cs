2283ef45e16486f1fb1dc11c2f857000740c4c0a
kishangdotnet <kishangdotnet@gmail.com>
2025-10-16 19:50:23 +0530

    Changed Controller Code, and Implemented Docker.

---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ProductAPI.Models;
+using System;
+namespace ProductAPI.Data
+{
+
+    public class ProductDbContext : DbContext
+    {
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+
+      
+        public class ProductDbContextFactory : IDesignTimeDbContextFactory<ProductDbContext>
+        {
+            public ProductDbContext CreateDbContext(string[] args)
+            {
+                // Load configuration from appsettings.json
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("ProductConnn");
+
+                var optionsBuilder = new DbContextOptionsBuilder<ProductDbContext>();
+                optionsBuilder.UseSqlServer(connectionString); 
+
+                return new ProductDbContext(optionsBuilder.Options);
+            }
+        }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,30 +1,107 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using ProductAPI.Data;
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+//// Add services to the container.
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+//    {
+//        Title = "Product API",
+//        Version = "v1"
+//    });
+//});
+
+//// Database connection
+//var connString = builder.Configuration.GetConnectionString("ProductConnn");
+
+//builder.Services.AddDbContext<ProductDbContext>(options =>
+//    options.UseSqlServer(connString));
+
+//var app = builder.Build();
+
+
+//// Enable Swagger in dev
+////if (app.Environment.IsDevelopment())
+////{
+////    app.UseSwagger(); // Generates /swagger/v1/swagger.json
+////    app.UseSwaggerUI(options =>
+////    {
+////        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+////    });
+////}
+
+//app.UseSwagger();
+//app.UseSwaggerUI();
+////var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container
+////builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+////builder.Services.AddSwaggerGen();
+
+////var app = builder.Build();
+
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+//app.UseAuthorization();
+//app.MapControllers();
+//app.Run();
+
 using Microsoft.EntityFrameworkCore;
 using ProductAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var dbName = builder.Configuration.GetConnectionString("MyTestDB") ?? "DefaultTestDb";
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase(dbName));
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Product API",
+        Version = "v1"
+    });
+});
+
+// Database connection
+var connString = builder.Configuration.GetConnectionString("ProductConnn");
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlServer(connString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Enable Swagger UI unconditionally (or restrict to development if you want)
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+});
+// }
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
